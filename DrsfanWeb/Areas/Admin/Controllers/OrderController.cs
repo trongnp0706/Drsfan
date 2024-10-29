f996cacd34200f3fcc440056eeaa91d489ff9924
trongnp0706 <phuoctrong1001@gmail.com>
2024-10-29 20:43:20 +0700

    Make Selected Status and Filter Order List

---
+++ DrsfanWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using DrsfanBook.DataAcess.Repository.IRepository;
+using DrsfanBook.Models;
+using DrsfanBook.Utility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DrsfanBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u=>u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.PaymentStatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
+            return Json(new { data = objOrderHeaders });
+        }
+
+
+        #endregion    }
+
+
+
+    }
+}
